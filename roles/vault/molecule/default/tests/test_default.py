f3d9e3165c57892aad7ae18bdebd03c5313de502
Artsiom Azarenka <a.azarenka@godeltech.com>
2019-06-28 09:47:33 +0300

    [DevOps] add unit tests

--- roles/vault/molecule/default/tests/test_default.py
+++ roles/vault/molecule/default/tests/test_default.py
@@ -5,10 +5,51 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+@pytest.fixture()
+def AnsibleDefaults():
+    with open("../../defaults/main.yml", 'r') as stream:
+        return yaml.load(stream)
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize("dirs", [
+    "/etc/vault.d",
+    "/usr/local/lib/vault/plugins",
+    "/var/vault",
+    "/var/log/vault",
+    "/var/run/vault",
+    "/home/vault"
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
 
+@pytest.mark.parametrize("files", [
+    "/etc/vault.d/vault_main.hcl",
+    "/lib/systemd/system/vault.service",
+    "/usr/local/bin/vault"
+])
+def test_files(host, files):
+    f = host.file(files)
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.is_file
+
+
+def test_user(host):
+    assert host.group("vault").exists
+    assert host.user("vault").exists
+
+
+def test_service(host):
+    s = host.service("vault")
+    assert s.is_enabled
+    assert s.is_running
+
+def test_socket(host, AnsibleDefaults):
+    addr = AnsibleDefaults['vault_address']
+    s = host.socket("tcp://addr:8200")
+    assert s.is_listening
+
+def test_version(host, AnsibleDefaults):
+    version = os.getenv('PROMETHEUS', AnsibleDefaults['prometheus_version'])
+    out = host.run("/usr/local/bin/vaul --version").stderr
+    assert "vault, version " + version in out
