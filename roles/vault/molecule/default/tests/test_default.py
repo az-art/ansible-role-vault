01d3b63f8e2ac26c046a321becf925c848e5c9c7
Artsiom Azarenka <a.azarenka@godeltech.com>
2019-06-28 10:00:59 +0300

    [DevOps] fix tests

--- roles/vault/molecule/default/tests/test_default.py
+++ roles/vault/molecule/default/tests/test_default.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 import yaml
-
+import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
